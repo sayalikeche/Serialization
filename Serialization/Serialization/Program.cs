0701dd7ccb8d3ee253fd9056dc2919535bc44252
sayalikeche <kechesayali11@gmail.com>
2021-12-09 22:07:17 +0530

    [sayali Added a Serialization And Deserialization]

---
+++ Serialization/Serialization/Program.cs
@@ -0,0 +1,19 @@
+﻿using Serialization;
+
+namespace Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySerialization binary = new BinarySerialization();
+            //binary.Serialization();
+            binary.Deserialization();
+            JsonSerialization serialization = new JsonSerialization();
+            serialization.JsonSerialization();
+            serialization.JsonDeserialize();
+        }
+    }
+
+
+}
